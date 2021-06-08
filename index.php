31d82f6913c00f8c875deb5c97ac9d11c1b095c1
wyhhky <60504940+wyhhky@users.noreply.github.com>
2021-06-08 21:59:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-print "CHANGE FROM GIT and SYNC to GOGS: Hello World with WEBHOOK setting !! PHP version is " . PHP_VERSION . "\n";
+print "REVISED FROM GIT and SYNC to GOGS: Hello World with WEBHOOK setting !! PHP version is " . PHP_VERSION . "\n";
 ?>
